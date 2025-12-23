99f1e1bc74f43f2b52b06afb94b106eed760ec48
Iggi-29 <ignasijs29@gmail.com>
2025-12-23 12:01:17 +0100

    gff3 to work only with autosomal and sexual chr

--- scriptsR/gff3_filtration.R
+++ scriptsR/gff3_filtration.R
@@ -13,13 +13,16 @@ gff <- readr::read_tsv(raw_gff3, skip = 7, col_names = FALSE)
 gff_header <- readLines(con = raw_gff3, n = 7)
 
 colnames(gff) <- c("chr","source_of_ann","type","start","end","dot1","strand","dunno","tag")
-
+valid_chrs <- c("chr1","chr2","chr3","chr4","chr5","chr6","chr7","chr8","chr9","chr10",
+                "chr11","chr12","chr13","chr14","chr15","chr16","chr17","chr18","chr19","chr20",
+                "chr21","chr22","chrX","chrY")
 ### filter the GFF3 file
 gff_filtered <- gff %>% 
   dplyr::rowwise() %>% 
   dplyr::mutate(keep = ifelse(type == "gene","YES, gene",
                               ifelse(grepl(x = tag, pattern = "Mane|MANE"),"YES, MANE","NO!"))) %>% 
-  dplyr::ungroup()
+  dplyr::ungroup() %>%
+  dplyr::filter(chr %in% valid_chrs)
 
 gff_filtered <- gff_filtered %>% 
   dplyr::filter(grepl(x = keep, pattern = "YES")) %>% 
